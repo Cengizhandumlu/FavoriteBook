387e0c96b03eaf83f03a00018978258265ff0c95
Cengizhan DUMLU <cengizhandumlu.35@gmail.com>
2021-04-09 23:06:08 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Cengizhan DUMLU on 9.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
